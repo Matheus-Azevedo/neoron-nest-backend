8cfec082a5640bbb3395cd80ac9bdb4b8d9036a7
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2024-09-13 15:17:47 -0300

    configured swagger and helmet

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,35 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, HttpStatus } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug', 'verbose'],
   });
 
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://neoron-next-frontned-f4dt.vercel.app/',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: HttpStatus.FORBIDDEN,
+  });
+
+  app.use(helmet()); // Helmet is a collection of 14 smaller middleware functions that set security-related HTTP headers, this will help secure the application
+
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Flights API')
+    .setDescription('The flights API for the Neoron project test.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3001);
 }
