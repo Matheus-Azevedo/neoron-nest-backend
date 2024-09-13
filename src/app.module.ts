2ae679d88e8df2786fd61b5f4f27d05df853aab9
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2024-09-13 09:26:46 -0300

    imports updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
+import { PrismaService } from './infra/database/prisma.service';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { FlightController } from './http/controllers/flight.controller';
+import { PrismaFlightRepository } from './repositories/prisma/prisma-flight.repository';
+import { FlightRepository } from './repositories/flight.repository';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [],
-  providers: [],
+  controllers: [FlightController],
+  providers: [
+    PrismaService,
+    {
+      provide: FlightRepository,
+      useClass: PrismaFlightRepository,
+    },
+  ],
 })
 export class AppModule {}
